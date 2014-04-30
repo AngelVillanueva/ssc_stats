de654e5b62b0a81d35adafdb18b95decd02c12c5
Angel Villanueva <avillanueva@sinapse.es>
2014-04-30 16:15:20 +0200

    Validates email format for Usuario

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -3,4 +3,7 @@ class Usuario < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+
 end
--- spec/models/usuario_spec.rb
+++ spec/models/usuario_spec.rb
@@ -7,4 +7,6 @@ describe Usuario do
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
   it { should validate_uniqueness_of :email }
+  it { should allow_value( "test@example.com" ).for( :email ) }
+  it { should_not allow_value( "foo" ).for( :email ) }
 end
