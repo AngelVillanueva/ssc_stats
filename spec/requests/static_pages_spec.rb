1524aa717aa08647cba992eaf0944dde506ef262
Angel Villanueva <avillanueva@sinapse.es>
2014-04-30 17:12:23 +0200

    Test logout link just for already login users

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
-  describe "are restricted to registered users" do
-    it "should ask for login" do
+  describe "are restricted" do
+    it "should ask for login to registered users" do
       get '/'
       response.status.should_not be(200)
       visit root_path
@@ -11,7 +11,7 @@ describe "StaticPages" do
     end
   end
 
-  describe "registered usuarios should be allowed" do
+  describe "not allow the entrance to non-registered users" do
     let( :usuario ) { FactoryGirl.create( :usuario ) }
     before do
       go_home_and_login
@@ -22,7 +22,7 @@ describe "StaticPages" do
     it { should have_title( "Santa Cristina | Inicio" ) }
   end
 
-  describe "registerd usuarios should be able to log out" do
+  describe "registered usuarios should be able to log out" do
     let( :usuario ) { FactoryGirl.create( :usuario ) }
     before do
       go_home_and_login
@@ -34,6 +34,13 @@ describe "StaticPages" do
       should have_css( "a.logout" )
       click_link I18n.t( 'devise.custom.sign_out' )
       should have_title( I18n.t("Restricted") )
+    end
+  end
+
+  describe "non-registered users cannot see the logout link" do
+    before { visit root_path }
+    it "should not be visible" do
+      expect( page ).to_not have_css( "a.logout" )
     end
   end
 
