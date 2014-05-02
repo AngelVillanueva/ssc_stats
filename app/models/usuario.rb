2302ef5105309baf63bfae23332f0ef7fc6bdd75
Angel Villanueva <avillanueva@sinapse.es>
2014-05-02 14:20:38 +0200

    Usuario not registerable (devise module off)

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,7 +1,7 @@
 class Usuario < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
