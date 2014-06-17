b08cb241279200b873f58b7a862d6b4edce921ac
Angel Villanueva <avillanueva@sinapse.es>
2014-06-17 16:20:18 +0200

    Match desired table and model names via inflectors

---
+++ spec/inflections/inflections_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe "Spanish inflections" do
+  it "test_plurales_que_terminar_en_n_o_s_con_acentos" do
+    expect( 'avión'.pluralize(:es) ).to eql( 'aviones' )
+    expect( 'interés'.pluralize(:es) ).to eql( 'intereses' )
+  end
+  it "para los nombres de las tablas" do
+    expect( 'Episodio'.tableize ).to eql( 'episodios' )
+    expect( 'Estancia'.tableize ).to eql( 'estancias' )
+    expect( 'Diagnostico'.tableize ).to eql( 'diagnosticos' )
+    expect( 'HonorarioMedico'.tableize ).to eql( 'honorarios_medicos' )
+    expect( 'VariosConTarifa'.tableize ).to eql( 'varios_con_tarifas' )
+    expect( 'VariosSinTarifa'.tableize ).to eql( 'varios_sin_tarifas' )
+    expect( 'Compañia'.tableize ).to eql( 'compañias' )
+    expect( 'Medico'.tableize ).to eql( 'medicos' )
+    expect( 'CodigoOmc'.tableize ).to eql( 'codigos_omc' )
+    expect( 'Precio'.tableize ).to eql( 'precios' )
+    expect( 'TipoCosteConTarifa'.tableize ).to eql( 'tipos_coste_con_tarifa' )
+    expect( 'SubtipoCoste'.tableize ).to eql( 'subtipos_coste' )
+    expect( 'TipoCosteSinTarifa'.tableize ).to eql( 'tipos_coste_sin_tarifa' )
+  end
+end
