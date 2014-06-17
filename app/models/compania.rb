770904d9f1ad3f9b7b8705cc4513d9ed1e9816cc
Angel Villanueva <avillanueva@sinapse.es>
2014-06-18 00:24:59 +0200

    Nombre as mandatory field in Compania

--- app/models/compania.rb
+++ app/models/compania.rb
@@ -1,3 +1,4 @@
 class Compania < ActiveRecord::Base
   has_many :precios
+  validates :nombre, presence: true
 end
--- spec/models/compania_spec.rb
+++ spec/models/compania_spec.rb
@@ -4,4 +4,6 @@ require 'spec_helper'
 describe Compania do
   it { should respond_to :nombre }
   it { should respond_to :precios }
+
+  it { should validate_presence_of :nombre } 
 end
