9992b581c2d0bfe9133423e8145859d21481b8ca
Angel Villanueva <avillanueva@sinapse.es>
2014-06-17 19:38:00 +0200

    Importe should be greater than or equal to 0

--- spec/models/precio_spec.rb
+++ spec/models/precio_spec.rb
@@ -5,6 +5,7 @@ describe Precio do
   it { should respond_to :vigencia }
   it { should respond_to :forfait? }
 
+  it { should validate_numericality_of(:importe).is_greater_than_or_equal_to(0) }
   it { should validate_numericality_of(:vigencia).is_greater_than_or_equal_to(2000) }
   it { should validate_numericality_of(:vigencia).is_less_than_or_equal_to(2030) }
 end
