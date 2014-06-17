7edb0b820f7e3a498ec3032b68f800ed4314171f
Angel Villanueva <avillanueva@sinapse.es>
2014-06-17 19:36:42 +0200

    Vigencia should be in 2000..2030 range

--- app/models/precio.rb
+++ app/models/precio.rb
@@ -1,2 +1,4 @@
 class Precio < ActiveRecord::Base
+  validates_numericality_of :vigencia, greater_than_or_equal_to: 2000
+  validates_numericality_of :vigencia, less_than_or_equal_to: 2030
 end
--- spec/models/precio_spec.rb
+++ spec/models/precio_spec.rb
@@ -4,4 +4,7 @@ describe Precio do
   it { should respond_to :importe }
   it { should respond_to :vigencia }
   it { should respond_to :forfait? }
+
+  it { should validate_numericality_of(:vigencia).is_greater_than_or_equal_to(2000) }
+  it { should validate_numericality_of(:vigencia).is_less_than_or_equal_to(2030) }
 end
