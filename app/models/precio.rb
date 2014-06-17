36420e89ad67060d3e5c413d7b1f82e948fe8f45
Angel Villanueva <avillanueva@sinapse.es>
2014-06-18 00:43:51 +0200

    Mandatory fields for Precio model

--- app/models/precio.rb
+++ app/models/precio.rb
@@ -3,6 +3,7 @@ class Precio < ActiveRecord::Base
   validates_numericality_of :importe, greater_than_or_equal_to: 0
   validates_numericality_of :vigencia, greater_than_or_equal_to: 2000
   validates_numericality_of :vigencia, less_than_or_equal_to: 2030
+  validates :importe, :vigencia, :incluido_en_forfait, :compania, presence: true
 
   rails_admin do
     configure :importe do
--- spec/models/precio_spec.rb
+++ spec/models/precio_spec.rb
@@ -6,6 +6,11 @@ describe Precio do
   it { should respond_to :incluido_en_forfait }
   it { should respond_to :compania }
 
+  it { should validate_presence_of :importe }
+  it { should validate_presence_of :vigencia}
+  it { should validate_presence_of :incluido_en_forfait }
+  it { should validate_presence_of :compania }
+
   it { should validate_numericality_of(:importe).is_greater_than_or_equal_to(0) }
   it { should validate_numericality_of(:vigencia).is_greater_than_or_equal_to(2000) }
   it { should validate_numericality_of(:vigencia).is_less_than_or_equal_to(2030) }
