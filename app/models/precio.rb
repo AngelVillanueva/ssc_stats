b39abbe65710b963cacb5ccd000e76bff2af78e2
Angel Villanueva <avillanueva@sinapse.es>
2014-06-18 11:35:19 +0200

    Extended options for Precio fields in rails_admin

--- app/models/precio.rb
+++ app/models/precio.rb
@@ -14,13 +14,32 @@ class Precio < ActiveRecord::Base
   validates_numericality_of :importe, greater_than_or_equal_to: 0
   validates_numericality_of :vigencia, greater_than_or_equal_to: 2000
   validates_numericality_of :vigencia, less_than_or_equal_to: 2030
-  validates :importe, :vigencia, :incluido_en_forfait, :compania, presence: true
+  validates :importe, :vigencia, :compania, presence: true
+
+  after_initialize do
+    if new_record?
+      self.incluido_en_forfait ||= true
+    end
+  end
 
   rails_admin do
+    # sorting the fields
+    field :importe
+    field :vigencia
+    field :compania
+    field :incluido_en_forfait
+    # extended options
     configure :importe do
       pretty_value do
         ActionController::Base.helpers.number_to_currency(value)
       end
+      help I18n.t("activerecord.attributes.precio.importe_help")
+    end
+    configure :vigencia do
+      help I18n.t("activerecord.attributes.precio.vigencia_help")
+    end
+    configure :incluido_en_forfait do
+      help I18n.t("activerecord.attributes.precio.incluido_en_forfait_help")
     end
   end
   
