08732378ddc01c51456c1384074c535c562f5e0a
Angel Villanueva <avillanueva@sinapse.es>
2014-06-18 13:05:53 +0200

    Concepto de Coste mandatory and unique

--- app/models/tipo_coste_con_tarifa.rb
+++ app/models/tipo_coste_con_tarifa.rb
@@ -1,3 +1,11 @@
 class TipoCosteConTarifa < ActiveRecord::Base
   has_many :precios
+  validates :concepto, presence: true
+  validates_uniqueness_of :concepto, case_sensitive: false
+
+  rails_admin do
+    edit do
+      field :concepto
+    end
+  end
 end
--- spec/models/tipo_coste_con_tarifa_spec.rb
+++ spec/models/tipo_coste_con_tarifa_spec.rb
@@ -2,4 +2,15 @@ require 'spec_helper'
 
 describe TipoCosteConTarifa do
   it { should respond_to :precios }
+  it { should validate_presence_of :concepto }
+  it { should validate_uniqueness_of :concepto }
+
+  describe "should validate concepto uniqueness in a non case sensitive way" do
+    let( :concepto_coste ) { FactoryGirl.create( :tipo_coste_con_tarifa ) }
+    let( :concepto_coste_2 ) { FactoryGirl.create( :tipo_coste_con_tarifa, concepto: "Otro" ) }
+    let( :concepto_nombre ) { concepto_coste.concepto.downcase }
+    before { concepto_coste_2.concepto = concepto_nombre }
+    subject { concepto_coste_2 }
+    it { should_not be_valid }
+  end
 end
