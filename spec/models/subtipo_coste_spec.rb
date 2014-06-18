e63cc9daecddd3a09fa607855fb2655464e469c9
Angel Villanueva <avillanueva@sinapse.es>
2014-06-18 16:12:58 +0200

    Show "Descripción" field in rails_admin for SubtipoCoste

--- spec/models/subtipo_coste_spec.rb
+++ spec/models/subtipo_coste_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
 
 describe SubtipoCoste do
+  it { should respond_to :tipos_coste_con_tarifa }
 end
--- spec/requests/tipo_coste_con_tarifa_admin_spec.rb
+++ spec/requests/tipo_coste_con_tarifa_admin_spec.rb
@@ -4,7 +4,8 @@ describe "Tipos de Coste con Tarifa management" do
   describe "authorized users should be able to do it" do
     let( :usuario ) { FactoryGirl.create( :usuario ) }
     let( :compania ) { FactoryGirl.create( :compania ) }
-    let( :concepto_de_coste ) { FactoryGirl.create( :tipo_coste_con_tarifa ) }
+    let( :subtipo ) { FactoryGirl.create( :subtipo_coste ) }
+    let( :concepto_de_coste ) { FactoryGirl.create( :tipo_coste_con_tarifa, subtipo_coste: subtipo ) }
     before { go_dashboard_and_login }
 
     it "by seeing the link" do
@@ -18,6 +19,12 @@ describe "Tipos de Coste con Tarifa management" do
       expect( concepto_de_coste ).to be_valid
       click_the_menu_link_for "tipo_coste_con_tarifa"
       expect( page ).to have_css( "td.concepto_field", text: "Electrocardiograma" )
+    end
+    it "by seeing its related SubtipoCoste" do
+      expect( compania ).to be_valid
+      expect( concepto_de_coste ).to be_valid
+      click_the_menu_link_for "tipo_coste_con_tarifa"
+      expect( page ).to have_css( "td.subtipo_coste_field", text: "Otros" )
     end
     it "by not seeing its Prices when editing" do
       click_the_menu_link_for "tipo_coste_con_tarifa"
