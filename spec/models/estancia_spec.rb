1e76079da0f09951cf6dd11d35960d32718290a1
Angel Villanueva <avillanueva@sinapse.es>
2014-06-26 10:53:11 +0200

    Limit TiposCoste to "Estancia" ones

--- spec/models/estancia_spec.rb
+++ spec/models/estancia_spec.rb
@@ -10,4 +10,15 @@ describe Estancia do
   it { should validate_presence_of :episodio }
   it { should validate_presence_of :tipo_coste_con_tarifa }
   it { should validate_numericality_of( :cantidad ).is_greater_than_or_equal_to( 1 ) }
+
+  describe "with a valid TipoCoste" do
+    let(:subtipo) { FactoryGirl.create( :subtipo_coste, descripcion: "Estancia" ) }
+    let(:tipo_coste) { FactoryGirl.create( :tipo_coste_con_tarifa, subtipo_coste: subtipo ) }
+    let(:tipo_coste_2) { FactoryGirl.create( :tipo_coste_con_tarifa, concepto: "Otro" ) }
+    let(:episodio) { FactoryGirl.create( :episodio ) }
+    let(:estancia ) { FactoryGirl.create( :estancia, tipo_coste_con_tarifa: tipo_coste ) }
+    subject { estancia }
+    before { estancia.tipo_coste_con_tarifa = tipo_coste_2 }
+    it { should_not be_valid }
+  end
 end
