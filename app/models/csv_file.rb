62064f5336ddf90b0b107af51a4b4c8bf257c318
Angel Villanueva <avillanueva@sinapse.es>
2014-06-27 16:39:30 +0200

    Mandatory :archivo in CsvFile

--- app/models/csv_file.rb
+++ app/models/csv_file.rb
@@ -1,7 +1,8 @@
 class CsvFile < ActiveRecord::Base
   has_attached_file :archivo,
     :path => ":rails_root/uploads/:class/:id/:basename.:extension"
-  validates_attachment :archivo, content_type: { content_type: [ "text/csv" ] }
+  validates_attachment :archivo, presence: true,
+    content_type: { content_type: ['text/csv','text/comma-separated-values','text/csv','application/csv','application/excel','application/vnd.ms-excel','application/vnd.msexcel','text/anytext','text/plain'] }
 
   rails_admin do
     edit do
--- spec/models/csv_file_spec.rb
+++ spec/models/csv_file_spec.rb
@@ -1,5 +1,23 @@
+# == Schema Information
+#
+# Table name: csv_files
+#
+#  id                   :integer          not null, primary key
+#  archivo_file_name    :string(255)
+#  archivo_content_type :string(255)
+#  archivo_file_size    :integer
+#  archivo_updated_at   :datetime
+#  created_at           :datetime
+#  updated_at           :datetime
+#
+
 require 'spec_helper'
 
 describe CsvFile do
   it { should respond_to :archivo }
+  describe "with mandatory Archivo file" do
+    let( :file ) { FactoryGirl.create( :csv_file ) }
+    before { file.archivo = nil }
+    it { should_not be_valid }
+  end
 end
