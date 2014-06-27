39e0c984c60c5c7e1b4ac274a33e11cd47fe523b
Angel Villanueva <avillanueva@sinapse.es>
2014-06-27 19:24:04 +0200

    Timestamps for TiposCosteConTarifa

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140627170721) do
+ActiveRecord::Schema.define(version: 20140627172031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,8 +80,10 @@ ActiveRecord::Schema.define(version: 20140627170721) do
   end
 
   create_table "tipos_coste_con_tarifa", force: true do |t|
-    t.string  "concepto"
-    t.integer "subtipo_coste_id"
+    t.string   "concepto"
+    t.integer  "subtipo_coste_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "tipos_coste_sin_tarifa", force: true do |t|
