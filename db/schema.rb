74bb3559309347ef528692b91f3c100b3581d3a9
Angel Villanueva <avillanueva@sinapse.es>
2014-06-27 17:03:28 +0200

    Timestamps for Episodios

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140627145700) do
+ActiveRecord::Schema.define(version: 20140627150041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,11 +32,13 @@ ActiveRecord::Schema.define(version: 20140627145700) do
   end
 
   create_table "episodios", force: true do |t|
-    t.integer "nhc"
-    t.date    "fecha_alta"
-    t.boolean "es_ambulante"
-    t.integer "compania_id"
-    t.integer "medico_id"
+    t.integer  "nhc"
+    t.date     "fecha_alta"
+    t.boolean  "es_ambulante"
+    t.integer  "compania_id"
+    t.integer  "medico_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "especialidades", force: true do |t|
