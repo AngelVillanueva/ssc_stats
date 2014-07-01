0c9b1854551713cce8e204a5e65812b6f46cadd4
Angel Villanueva <avillanueva@sinapse.es>
2014-07-01 12:34:55 +0200

    Initial test for duplicated record on Import

--- spec/features/rails_admin_import_spec.rb
+++ spec/features/rails_admin_import_spec.rb
@@ -14,9 +14,10 @@ describe "Import as an option for certains models" do
     expect( page ).to have_css( "input[type='file'].archivo" )
   end
   it "creates a temporary CsvFile record by uploading a csv file" do
+    a_file = create_csv_file_for 2, Compania
     click_the_menu_link_for "compania"
     click_the_action_link_for "import"
-    attach_file "_admin_compania_import_archivo", "#{Rails.root}/spec/fixtures/test.csv"
+    attach_file "_admin_compania_import_archivo", a_file
     expect{ click_save_button }.to change{ CsvFile.count }.by(1)
   end
   it "creates two Compania records if the file contains those data" do
@@ -27,14 +28,32 @@ describe "Import as an option for certains models" do
     expect{ click_save_button }.to change{ Compania.count }.by(2)
     expect( page ).to have_css( ".alert-success", text: I18n.t("exitos.messages.created_records", records: 2) )
   end
+  it "should advise when not all the rows created a new record" do
+    a_file = create_csv_file_for 2, Compania, true
+    click_the_menu_link_for "compania"
+    click_the_action_link_for "import"
+    attach_file "_admin_compania_import_archivo", a_file
+    expect{ click_save_button }.to change{ Compania.count }.by(1)
+    expect( page ).to have_css( ".alert-success", text: I18n.t("exitos.messages.created_records", records: 1) )
+    expect( page ).to have_css( ".alert-error", text: I18n.t("errors.messages.created_records", records: 1) )
+  end
 end
 
-def create_csv_file_for records, model
+def create_csv_file_for records, model, repeated = false
   the_file = "#{Rails.root}/spec/fixtures/test.csv"
   File.open(the_file, 'w') do |file| 
-    file.puts "\"nombre\""
-    file.puts "\"Adeslas\""
-    file.puts "\"Asisa\""
+    file_content_for(model.to_s, repeated).each do |line_item|
+      file.puts line_item
+    end
   end
   the_file
+end
+
+def file_content_for model, repeated = false
+  case model
+    when "Compania" then
+      line_items = repeated ? %w( "nombre" "Adeslas" "Adeslas" ) : %w( "nombre" "Adeslas" "Asisa" )
+    else line_items = []
+  end
+  line_items
 end
