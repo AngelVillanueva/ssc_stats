55cdfaea0275af4d2b2d9c3a08021cfa90fe5a2e
Angel Villanueva <avillanueva@sinapse.es>
2014-07-02 13:28:20 +0200

    Refactoring create_from_import as shared Concern

--- app/models/compania.rb
+++ app/models/compania.rb
@@ -9,31 +9,32 @@
 #
 
 class Compania < ActiveRecord::Base
+  include Shared::Import
   has_many :precios
   has_many :episodios
   validates :nombre, presence: true
   validates_uniqueness_of :nombre, case_sensitive: false
 
-  def self.create_from_import file_path
-    resultado = {:creados => 0, :fallados => 0, :errors => []}
-    creados = 0
-    fallados = 0
-    CSV.foreach( file_path, headers: true ) do |row|
-      object = self.new row.to_hash
-      if object.valid?
-        object.save!
-        creados = creados + 1
-      else
-        object.errors.messages.each do |k,v|
-          resultado[:errors] << "#{k.to_s.titleize}: #{v.join(',')}"
-        end
-        fallados += 1
-      end
-    end
-    resultado[:creados] = creados
-    resultado[:fallados] = fallados
-    resultado
-  end
+  # def self.create_from_import file_path
+  #   resultado = {:creados => 0, :fallados => 0, :errors => []}
+  #   creados = 0
+  #   fallados = 0
+  #   CSV.foreach( file_path, headers: true ) do |row|
+  #     object = self.new row.to_hash
+  #     if object.valid?
+  #       object.save!
+  #       creados = creados + 1
+  #     else
+  #       object.errors.messages.each do |k,v|
+  #         resultado[:errors] << "#{k.to_s.titleize}: #{v.join(',')}"
+  #       end
+  #       fallados += 1
+  #     end
+  #   end
+  #   resultado[:creados] = creados
+  #   resultado[:fallados] = fallados
+  #   resultado
+  # end
 
   rails_admin do
     edit do
---
+++ app/models/concerns/shared/import.rb
@@ -0,0 +1,32 @@
+module Shared::Import
+  extend ActiveSupport::Concern
+
+  # methods defined here are going to extend the class, not the instance of it
+  module ClassMethods
+
+    def create_from_import file_path
+      # initial values for response array
+      resultado = {:creados => 0, :fallados => 0, :errors => []}
+      creados = 0
+      fallados = 0
+      # iterate through CSV file rows trying to create new records
+      CSV.foreach( file_path, headers: true ) do |row|
+        object = self.new row.to_hash
+        if object.valid?
+          object.save!
+          creados = creados + 1
+        else
+          object.errors.messages.each do |k,v|
+            resultado[:errors] << "#{k.to_s.titleize}: #{v.join(',')}"
+          end
+          fallados += 1
+        end
+      end
+      # response including errors if any
+      resultado[:creados] = creados
+      resultado[:fallados] = fallados
+      resultado
+    end
+  end
+
+end
