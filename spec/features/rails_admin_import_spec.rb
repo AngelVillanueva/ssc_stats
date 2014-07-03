a67f06173694cd54f3bfae272151f7485c9d355e
Angel Villanueva <avillanueva@sinapse.es>
2014-07-03 16:12:48 +0200

    Refactoring tests

--- spec/features/rails_admin_import_spec.rb
+++ spec/features/rails_admin_import_spec.rb
@@ -2,60 +2,68 @@ require 'spec_helper'
 
 describe "Import as an option for certains models" do
   let( :usuario ) { FactoryGirl.create( :usuario ) }
+  let( :models ) { [ SubtipoCoste, Compania, Especialidad ] }
   before { go_dashboard_and_login }
 
-  it "available for Compañías" do
-    click_the_menu_link_for "compania"
-    expect( page ).to have_css( "li.import_collection_link" )
-  end
-  it "available for Especialidades" do
-    click_the_menu_link_for "especialidad"
-    expect( page ).to have_css( "li.import_collection_link" )
+  it "available for certain models" do
+    models.each do |model|
+      click_the_menu_link_for model.to_s.underscore
+      expect( page ).to have_css( "li.import_collection_link" )
+    end
   end
   it "asks for a CSV file to import" do
-    click_the_menu_link_for "compania"
-    click_the_action_link_for "import"
-    expect( page ).to have_css( "input[type='file'].archivo" )
+    models.each do |model|
+      click_the_menu_link_for model.to_s.underscore
+      click_the_action_link_for "import"
+      expect( page ).to have_css( "input[type='file'].archivo" )
+    end
   end
   it "creates a temporary CsvFile record by uploading a csv file" do
-    upload_a_file_for 2, Compania
-    expect{ click_save_button }.to change{ CsvFile.count }.by(1)
+    models.each do |model|
+      upload_a_file_for 2, model
+      expect{ click_save_button }.to change{ CsvFile.count }.by(1)
+    end
   end
   it "should advise when the CsvFile format is wrong" do
-    upload_a_bad_file_for Compania
-    expect{ click_save_button }.to_not change{ CsvFile.count }
-    expect( Compania.count ).to eql 0
-    expect( page ).to have_css( ".alert-error", text: I18n.t( "errors.messages.invalid" ) )
+    models.each do |model|
+      upload_a_bad_file_for model
+      expect{ click_save_button }.to_not change{ CsvFile.count }
+      expect( model.count ).to eql 0
+      expect( page ).to have_css( ".alert-error", text: I18n.t( "errors.messages.invalid" ) )
+    end
   end
-  it "creates two Compania records if the file contains those data" do
-    upload_a_file_for 2, Compania
-    expect{ click_save_button }.to change{ Compania.count }.by(2)
-    expect( page ).to have_css( ".alert-success", text: I18n.t("exitos.messages.created_records", records: 2) )
-  end
-  it "creates two Especialidad records if the file contains those data" do
-    upload_a_file_for 2, Especialidad
-    expect{ click_save_button }.to change{ Especialidad.count }.by(2)
-    expect( page ).to have_css( ".alert-success", text: I18n.t("exitos.messages.created_records", records: 2) )
+  it "creates two Model records if the file contains those data" do
+    models.each do |model|
+      upload_a_file_for 2, model
+      expect{ click_save_button }.to change{ model.count }.by(2)
+      expect( page ).to have_css( ".alert-success", text: I18n.t("exitos.messages.created_records", records: 2) )
+    end
   end
   it "should advise when not all the rows created a new record" do
-    upload_a_file_for 2, Compania, "repetido"
-    expect{ click_save_button }.to change{ Compania.count }.by(1)
-    expect( page ).to have_css( ".alert-success", text: I18n.t("exitos.messages.created_records", records: 1) )
-    expect( page ).to have_css( ".alert-error", text: I18n.t("errors.messages.created_records", records: 1) )
+    models.each do |model|
+      upload_a_file_for 2, model, "repetido"
+      expect{ click_save_button }.to change{ model.count }.by(1)
+      expect( page ).to have_css( ".alert-success", text: I18n.t("exitos.messages.created_records", records: 1) )
+      expect( page ).to have_css( ".alert-error", text: I18n.t("errors.messages.created_records", records: 1) )
+    end
   end
   it "should advise when the row headers do not match the model attributes" do
-    upload_a_file_for 2, Compania, "wrong_headers"
-    expect{ click_save_button }.to change{ Compania.count }.by(0)
-    expect( page ).to have_css( ".alert-success", text: I18n.t("exitos.messages.created_records", records: 0) )
-    expect( page ).to have_css( ".alert-error", text: I18n.t("errors.messages.no_created_records" ) )
-    expect( page ).to have_css( ".alert-notice", text: I18n.t("errors.messages.bad_row_headers" ) )
+    models.each do |model|
+      upload_a_file_for 2, model, "wrong_headers"
+      expect{ click_save_button }.to change{ model.count }.by(0)
+      expect( page ).to have_css( ".alert-success", text: I18n.t("exitos.messages.created_records", records: 0) )
+      expect( page ).to have_css( ".alert-error", text: I18n.t("errors.messages.no_created_records" ) )
+      expect( page ).to have_css( ".alert-notice", text: I18n.t("errors.messages.bad_row_headers" ) )
+    end
   end
   it "should ignore the file content when rows do not match all the model attributes" do
-    upload_a_mixed_file_for Compania
-    expect{ click_save_button }.to change{ Compania.count }.by(0)
-    expect( page ).to have_css( ".alert-success", text: I18n.t("exitos.messages.created_records", records: 0) )
-    expect( page ).to have_css( ".alert-error", text: I18n.t("errors.messages.no_created_records" ) )
-    expect( page ).to have_css( ".alert-notice", text: I18n.t("errors.messages.bad_row_headers" ) )
+    models.each do |model|
+      upload_a_mixed_file_for model
+      expect{ click_save_button }.to change{ model.count }.by(0)
+      expect( page ).to have_css( ".alert-success", text: I18n.t("exitos.messages.created_records", records: 0) )
+      expect( page ).to have_css( ".alert-error", text: I18n.t("errors.messages.no_created_records" ) )
+      expect( page ).to have_css( ".alert-notice", text: I18n.t("errors.messages.bad_row_headers" ) )
+    end
   end
 end
 
@@ -63,23 +71,23 @@ end
 
 def upload_a_bad_file_for model
   a_file = "#{Rails.root}/spec/fixtures/test.jpg"
-  click_the_menu_link_for model.to_s.downcase
+  click_the_menu_link_for model.to_s.underscore
   click_the_action_link_for "import"
-  attach_file "_admin_#{model.to_s.downcase}_import_archivo", a_file
+  attach_file "_admin_#{model.to_s.underscore}_import_archivo", a_file
 end
 
 def upload_a_mixed_file_for model
   a_file = "#{Rails.root}/spec/fixtures/test_mixed.csv"
-  click_the_menu_link_for model.to_s.downcase
+  click_the_menu_link_for model.to_s.underscore
   click_the_action_link_for "import"
-  attach_file "_admin_#{model.to_s.downcase}_import_archivo", a_file
+  attach_file "_admin_#{model.to_s.underscore}_import_archivo", a_file
 end
 
 def upload_a_file_for records, model, type = "normal"
   a_file = create_csv_file_for records, model, type
-  click_the_menu_link_for model.to_s.downcase
+  click_the_menu_link_for model.to_s.underscore
   click_the_action_link_for "import"
-  attach_file "_admin_#{model.to_s.downcase}_import_archivo", a_file
+  attach_file "_admin_#{model.to_s.underscore}_import_archivo", a_file
 end
 
 def create_csv_file_for records, model, type = "normal"
@@ -94,6 +102,12 @@ end
 
 def file_content_for model, type = "normal"
   case model
+    when "SubtipoCoste"
+      case type
+        when "repetido" then line_items = %w( "descripcion" "Estancia" "Estancia" )
+        when "wrong_headers" then line_items = %w( "nombre" "Estancia" "Quirófano" )
+        else line_items = %w( "descripcion" "Estancia" "Quirófano" )
+      end
     when "Compania"
       case type
         when "repetido" then line_items = %w( "nombre" "Adeslas" "Adeslas" )
@@ -103,6 +117,7 @@ def file_content_for model, type = "normal"
     when "Especialidad"
       case type
         when "repetido" then line_items = %w( "nombre" "Diagnosis" "Diagnosis" )
+        when "wrong_headers" then line_items = %w( "concepto" "Diagnosis" "Alergia" )
         else line_items = %w( "nombre" "Diagnosis" "Alergia" )
       end
     else line_items = [""]
