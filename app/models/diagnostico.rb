688eb3b28bb466774a6506f703d2d212f631e30f
Angel Villanueva <avillanueva@sinapse.es>
2014-07-04 16:47:31 +0200

    Rails_admin labels and views for Diagnostico

--- app/models/diagnostico.rb
+++ app/models/diagnostico.rb
@@ -14,4 +14,17 @@ class Diagnostico < ActiveRecord::Base
   belongs_to :codigo_omc
 
   validates_with TipoValidator, nombres: %w( Diagnóstico Quirófano Quirofano ) # check that the TipoCoste fits the Subtipo
+
+  rails_admin do
+    list do
+      field :id
+      field :episodio
+      field :codigo_omc
+      field :tipo_coste_con_tarifa do
+        label I18n.t("activerecord.attributes.diagnostico.tipo_coste_con_tarifa_id")
+      end
+      field :created_at
+      field :updated_at
+    end
+  end
 end
