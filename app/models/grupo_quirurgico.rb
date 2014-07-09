090f1156761a5aa292715d57c829daa03de44d1b
Angel Villanueva <avillanueva@sinapse.es>
2014-07-09 13:05:54 +0200

    Field :nombre as mandatory, unique in Grupo Q

--- app/models/grupo_quirurgico.rb
+++ app/models/grupo_quirurgico.rb
@@ -1,2 +1,13 @@
 class GrupoQuirurgico < ActiveRecord::Base
+  validates :nombre, presence: true, uniqueness: true
+
+  rails_admin do
+    list do
+      field :id
+      field :nombre
+      field :codigos_omc
+      field :created_at
+      field :updated_at
+    end
+  end
 end
--- spec/models/grupo_quirurgico_spec.rb
+++ spec/models/grupo_quirurgico_spec.rb
@@ -2,4 +2,7 @@ require 'spec_helper'
 
 describe GrupoQuirurgico do
   it { should respond_to :nombre }
+
+  it { should validate_presence_of :nombre }
+  it { should validate_uniqueness_of :nombre }
 end
